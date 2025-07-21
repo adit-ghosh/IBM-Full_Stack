4d2513d95b8a7d6a63409f215d6eaeb826db5cdf
Adit Ghosh <aditghosh5112005@gmail.com>
2025-07-21 19:20:29 +0530

    minor changes

--- React/paradise-nursery/vite.config.js
+++ React/paradise-nursery/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ibm/react/nursery-paradise/', // Must match your GitHub Pages path
+ base: '/IBM-Full_Stack/React/paradise-nursery/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
-}
+  }
 })
